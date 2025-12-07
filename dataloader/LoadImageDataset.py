ebaf55c35681bddcfe9bac8c44b273aebd1e6490
Bahri ABACI <abacibahri@gmail.com>
2025-12-07 12:18:37 +0300

    train_test ratio read from dataset configuration

--- dataloader/LoadImageDataset.py
+++ dataloader/LoadImageDataset.py
@@ -33,6 +33,7 @@ class LoadImageDataset:
             
             # add the necessary configuration options
             self.name = config["dataset_name"]
+            self.train_test_ratio  = config["train_test_ratio"]
             self.resize_after_load = config["resize_after_load"]
             self.crop_after_resize = config["crop_after_resize"]
             self.image_count = len(self.rgb_paths)
@@ -120,3 +121,6 @@ class LoadImageDataset:
             return self.crop_after_resize
         else:
             return self.resize_after_load
+
+    def GetTrainTestRatio(self):
+        return self.train_test_ratio
--- dataloader/NoiseImageDataLoader.py
+++ dataloader/NoiseImageDataLoader.py
@@ -10,7 +10,6 @@ class NoiseImageDataLoader:
         # dataset options
         self.noise_count = 10000                    # total noise count
         self.random_seed = 12237505                 # random seed for the tests
-        self.train_test_ratio_img = [0.7, 0.3]      # train and test ratio for the image dataset
         self.train_test_ratio_fpn = [0.9, 0.1]      # train and test ratio for the fpn dataset
         self.aggregation_size = aggregation_size    # number of noisy image for each FPN pattern
         self.current_idx = 0                        # set the current to zero
@@ -32,7 +31,10 @@ class NoiseImageDataLoader:
         # dataset should have __init__, __get_item__ and __len__ methods
         self.image_dataset = LoadImageDataset(dataset_config, self.vh_flip, self.crop_random, self.device)
         self.fpn_dataset   = LoadFPNDataset(self.image_dataset.GetImageSize(), self.noise_count, self.device)
-        
+
+        # get the split ratio from config
+        self.train_test_ratio_img = self.image_dataset.GetTrainTestRatio()
+
         # create shuffled image indices
         image_indices = list(range(len(self.image_dataset)))
         random.shuffle(image_indices)
