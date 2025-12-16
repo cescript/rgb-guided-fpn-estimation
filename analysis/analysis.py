e66d51d3a64bbfe69dc326ea106671b0dd36421f
Bahri ABACI <abacibahri@gmail.com>
2025-12-16 23:34:56 +0300

    analysis code updated

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -3,39 +3,48 @@ import matplotlib.pyplot as plt
 from collections import defaultdict
 
 # read the data and return algorithm_names vs scores
-def read_scores(filename, sort_result=True, ignore_safta=False):
-    with open(filename, 'r') as f:
-        lines = [line.strip() for line in f]
+def read_scores(logfile, append_k=0):
+    # get each line from file content
+    content = open(logfile, "r")
+    lines = [line.strip() for line in content]
     
     # get the metric names from the first line
     metric_names = lines[0][1:].split(',')[1:]
     metric_names = [m.strip() for m in metric_names]
-    
+
     # extract algorithm names and scores
     raw_data = []
     for i in range(0, len(lines), 2):
         name = lines[i][1:].split(',')[0].strip()
-        
-        # ignore safta and use SAFTA-RGB as SAFTA
-        if ignore_safta:
-            if name == "SAFTA":
-                continue
-            name = name.replace("SAFTA-RGB", "SAFTA")
+        # make SAFTA, SAFTA-RGB conversion
+        if name == "SAFTA":
+            name = "SAFTA-NONE"
 
+        # append K into algorithm name
+        if append_k > 0:
+            name = f"{name}K{append_k}"
+
+        # push name and values
         values = [float(v.strip()) for v in lines[i + 1].split(',')]
         raw_data.append((name, values))
-    
-    # sort by first metric in descending order
-    if sort_result:
-        raw_data.sort(key=lambda x: x[1][0], reverse=True)
-    
+
     algorithm_names = [name for name, _ in raw_data]
     scores = {m: [] for m in metric_names}
     for _, values in raw_data:
         for j, m in enumerate(metric_names):
             scores[m].append(values[j])
-    
+
     return algorithm_names, scores
+
+def write_score_table_txt(filename, algorithm_names, scores, metrics=("psnr", "ssim", "gmsd")):
+    with open(filename, "w") as f:
+        # header of the table
+        f.write(",".join(["metric"] + algorithm_names) + "\n")
+        # rows are the scores
+        for m in metrics:
+            values = scores[m]
+            row = [m] + [f"{v:.3f}" for v in values]
+            f.write(",".join(row) + "\n")
 
 # save figures for selected metric
 def save_figures(algorithm_names, scores, selected_metric, title, rotation_angle=30):
@@ -54,19 +63,16 @@ def save_figures(algorithm_names, scores, selected_metric, title, rotation_angle
                 ha='center', va='top', fontsize=12, color='#f7fcb9')
     
     fig.tight_layout()
-    figure_name = os.path.join("output", f"{title.replace(' ', '_').lower()}_{selected_metric}.png")
-    fig.savefig(figure_name, dpi=300)
+    figure_path = os.path.join("output", "figures")
+    filename = f"{title.replace(' ', '_').lower()}_{selected_metric}"
 
-# for ablation study to look better
-def format_algorithm_label(code):
-    parts = code.split('_')
-    names = {"rgb": "RGB", "irn": "IRN", "fpn": "FPN"}
-    symbols = {"1": "✓", "0": "✗"}
+    # create output directory for the images and logs
+    if not os.path.exists(figure_path):
+        os.makedirs(figure_path)
 
-    lines = []
-    for part in parts:
-        lines.append(f"{names[part[:-1]]}: {symbols[part[-1]]}")
-    return "\n".join(lines)
+    # dump image and log
+    write_score_table_txt(os.path.join(figure_path, f"{filename}.txt"), algorithm_names, scores)
+    fig.savefig(os.path.join(figure_path, f"{filename}.png"), dpi=300)
 
 def print_formatted(algorithm_names_and_k, scores, selected_metric, title):
     # split algorithm names and k's
@@ -83,53 +89,56 @@ def print_formatted(algorithm_names_and_k, scores, selected_metric, title):
         print(f"{aname}: ", end=' & ')
         sorted_data = sorted(grouped[aname], key=lambda d: d["k"])
         for item in sorted_data:
-            print(f"{item['score']: .2f}", end=' & ')
+            print(f"{item['score']: .3f}", end=' & ')
         print("")
     
 # draw figures for all tests
 if __name__ == '__main__':
-    selected_metric = "psnr"
-    
+
+    ################# DO COMPARISON ANALYSIS ON M3FD #################
     # get the results for FPN
-    title = "Model Performance for FPN"
-    algorithm_names, scores = read_scores("output/model_comparison/scores.txt", ignore_safta=True)
-    save_figures(algorithm_names, scores, selected_metric, title)
+    title = "Model Performance for FPN on M3FD"
+    logpath = "output/comparison/m3fd_config_K_12_fpn/scores.txt"
+    if os.path.isfile(logpath):
+        algorithm_names, scores = read_scores(logpath)
+        save_figures(algorithm_names, scores, "psnr", title)
 
     # get the results for HFN
-    title = "Model Performance for HFN"
-    algorithm_names, scores = read_scores("output/model_comparison_hfn/scores.txt", ignore_safta=True)
-    save_figures(algorithm_names, scores, selected_metric, title)
+    title = "Model Performance for HFN on M3FD"
+    logpath = "output/comparison/m3fd_config_K_12_hfn/scores.txt"
+    if os.path.isfile(logpath):
+        algorithm_names, scores = read_scores(logpath)
+        save_figures(algorithm_names, scores, "psnr", title)
 
+    ################# DO COMPARISON ANALYSIS ON MSRS #################
+    # get the results for FPN
+    title = "Model Performance for FPN on MSRS"
+    logpath = "output/comparison/msrs_config_K_12_fpn/scores.txt"
+    if os.path.isfile(logpath):
+        algorithm_names, scores = read_scores(logpath)
+        save_figures(algorithm_names, scores, "psnr", title)
+
+    # get the results for HFN
+    title = "Model Performance for HFN  on MSRS"
+    logpath = "output/comparison/msrs_config_K_12_hfn/scores.txt"
+    if os.path.isfile(logpath):
+        algorithm_names, scores = read_scores(logpath)
+        save_figures(algorithm_names, scores, "psnr", title)
+
+    ################# DO ABLATION ANALYSIS #################
     # get the results for FPN
     title = "Ablation Performance on FPN"
-    algorithm_names, scores = read_scores("output/ablation_study/scores.txt")
-    algorithm_names = [format_algorithm_label(n) for n in algorithm_names]
-    save_figures(algorithm_names, scores, selected_metric, title, 0)
+    logpath = "output/ablation/m3fd_config_K_12_fpn/scores.txt"
+    if os.path.isfile(logpath):
+        algorithm_names, scores = read_scores(logpath)
+        save_figures(algorithm_names, scores, "psnr", title, 0)
 
     # get the results for HFN
     title = "Ablation Performance on HFN"
-    algorithm_names, scores = read_scores("output/ablation_study_hfn/scores.txt")
-    algorithm_names = [format_algorithm_label(n) for n in algorithm_names]
-    save_figures(algorithm_names, scores, selected_metric, title, 0)
+    logpath = "output/ablation/m3fd_config_K_12_hfn/scores.txt"
+    if os.path.isfile(logpath):
+        algorithm_names, scores = read_scores(logpath)
+        save_figures(algorithm_names, scores, "psnr", title, 0)
 
-    # get the results for FPN
-    title = "OLS vs FPN Estimator Performance for FPN"
-    algorithm_names, scores = read_scores("output/ols_comparison/scores.txt", ignore_safta=True)
-    save_figures(algorithm_names, scores, selected_metric, title)
-
-    # get the results for HFN
-    title = "OLS vs FPN Estimator Performance for HFN"
-    algorithm_names, scores = read_scores("output/ols_comparison_hfn/scores.txt", ignore_safta=True)
-    save_figures(algorithm_names, scores, selected_metric, title)
-    
-    # get the results for FPN vs K
-    title = "Effect of Sequence Length on FPN"
-    algorithm_names, scores = read_scores("output/aggregation_comparison/scores.txt", sort_result=False, ignore_safta=True)
-    print_formatted(algorithm_names, scores, selected_metric, title)
-
-    # get the results for HFN
-    title = "Effect of Sequence Length on HFN"
-    algorithm_names, scores = read_scores("output/aggregation_comparison_hfn/scores.txt", sort_result=False, ignore_safta=True)
-    print_formatted(algorithm_names, scores, selected_metric, title)
     
     
