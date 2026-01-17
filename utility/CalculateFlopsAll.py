06032dec6bd6ec133ee2ea592086554f83914af9
Bahri ABACI <abacibahri@gmail.com>
2026-01-17 13:07:55 +0300

    calculate flops updated

--- utility/CalculateFlopsAll.py
+++ utility/CalculateFlopsAll.py
@@ -1,14 +1,16 @@
 import torch
 import models
+from utility.GetDevice import GetDevice
 from torch.utils.flop_counter import FlopCounterMode
 
 # set the model names to be evaluated
-model_names = ["EMPTY", "MULTIVIEW", "DLSNUC", "SAFTA", "SAFTA-RGB", "D1WLS"]
+model_names = ["SAFTA-RGB", "SAFTA-RGB-NIL"]
 
 # get flops for each model
-K = 8
-inp_rgb = [torch.randn((3, 288, 288)) for _ in range(K)]
-inp_irn = [torch.randn((1, 288, 288)) for _ in range(K)]
+K = 12
+device = GetDevice()
+inp_rgb = [torch.randn((3, 288, 288), device=device) for _ in range(K)]
+inp_irn = [torch.randn((1, 288, 288), device=device) for _ in range(K)]
 
 # loop over all models and test the results
 for mid, model_name in enumerate(model_names):
