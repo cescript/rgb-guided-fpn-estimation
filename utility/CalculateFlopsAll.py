9cf8864428afd566ae77be65e8f834810eacb1fe
Bahri ABACI <abacibahri@gmail.com>
2026-03-28 13:50:55 +0300

    calculateflops updated to print parameters as well

--- utility/CalculateFlopsAll.py
+++ utility/CalculateFlopsAll.py
@@ -4,7 +4,7 @@ from utility.GetDevice import GetDevice
 from torch.utils.flop_counter import FlopCounterMode
 
 # set the model names to be evaluated
-model_names = ["SAFTA-RGB", "SAFTA-RGB-NIL"]
+model_names = ["SAFTA-RGB", "DCGAN", "MULTIVIEW", "D1WLS", "DLSNUC", "EMPTY"]
 
 # get flops for each model
 K = 12
@@ -17,10 +17,18 @@ for mid, model_name in enumerate(model_names):
     # create a model given opt.model and other options
     model = models.GenerateModel(model_name)
 
+    # after model creation
+    num_params = 0
+    for attr_name in dir(model):
+        attr = getattr(model, attr_name)
+        if isinstance(attr, torch.nn.Module):
+            num_params += sum(p.numel() for p in attr.parameters())
+            # print(f"  {attr_name}: {num_params / 1e3:.1f}K params")
+
     # measure the flops
     flop_counter = FlopCounterMode(display=False, depth=None)
     with flop_counter:
         model.forward(inp_irn, inp_rgb)
 
     total_flops = flop_counter.get_total_flops()
-    print("%10s \t FLOPs: %.2f GFlops" % (model_name, total_flops / (K*1e9)))
+    print("%10s \t Params: %.2fK \t FLOPs: %.2f GFlops" % (model_name, num_params / 1e3, total_flops / (K * 1e9)))
